92432ea51765054f06a35e182c6c5f31656df996
Evan Giese <giesee@mymacewan.ca>
2017-03-10 16:18:30 -0700

    Navigation arrows are better located

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -7,12 +7,26 @@ import * as d3 from "d3";
 
 
 const LArrow = {
-    height: "25%",
-    width: "16.66%"
+    width: "16.66%",
+    position: "fixed",
+    left:"0",
+    bottom:"50%",
+    top:"30%"
 }
 const RArrow = {
-    height: "25%",
-    width: "16.66%"
+    width: "16.66%",
+    position: "fixed",
+    right:"0",
+    bottom:"50%",
+    top:"30%"
+}
+
+const content = {
+width:"66.66%", 
+overflow: "scroll", 
+height:"500px",
+marginLeft: "16.6%",
+marginRight: "16.6%",
 }
 
 
@@ -21,14 +35,15 @@ class Header extends Component{
   render() {
     return (
         <div>
-        <Link to={"Scheduler"}>
-            <img src="leftArrow.png" style={LArrow}/>
-        </Link>
-        <div style={{width:"66.66%", overflow: "scroll", height:"300px"}}>{this.props.children}</div>
-        <Link to={"Scheduler"}>
-            <img src="RightArrow.png" style={RArrow}/>
-        </Link>
+            <Link to={"Scheduler"}>
+                <img src="leftArrow.png" style={LArrow}/>
+            </Link>
+            <div style={content}>{this.props.children}</div>
+            <Link to={"Scheduler"}>
+                <img src="RightArrow.png" style={RArrow}/>
+            </Link>
         </div>
+        
     )
   }
 };
