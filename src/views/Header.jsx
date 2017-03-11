5907e49888fecc4518d33b1cd2fb75db4b0b7801
Evan Giese <giesee@mymacewan.ca>
2017-03-11 15:19:49 -0700

    add some nice buttons

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -2,9 +2,9 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import ScatterplotMatrix from "./ScatterplotMatrix"
-import "./Navigation.scss"
 import * as d3 from "d3";
-
+import {Button, ButtonGroup,  DropdownButton, MenuItem} from "react-bootstrap"
+import '../style/bootstrap/bootstrap.scss';
 
 const LArrow = {
     width: "16.66%",
@@ -29,17 +29,33 @@ marginLeft: "16.6%",
 marginRight: "16.6%",
 }
 
+const NavBar = {
+    width: "100%",
+    position: "fixed",
+    background
 
+};
 class Header extends Component{
 
   render() {
     return (
         <div>
+            <div style={NavBar}>
+                <ButtonGroup className="bootstrap">
+                    <DropdownButton title="Dropdown" id="bg-nested-dropdown" className="bootstrap">
+                        <MenuItem eventKey="1" className="bootstrap">Dropdown link</MenuItem>
+                        <MenuItem eventKey="2" className="bootstrap">Dropdown link</MenuItem>
+                    </DropdownButton>
+                    <Button className="bootstrap">{"<-"}</Button>
+                    <Button className="bootstrap">{"->"}</Button>
+                </ButtonGroup>
+            </div>
+
             <Link to={"Scheduler"}>
                 <img src="leftArrow.png" style={LArrow}/>
             </Link>
             <div style={content}>{this.props.children}</div>
-            <Link to={"Scheduler"}>
+            <Link to={"Henry"}>
                 <img src="RightArrow.png" style={RArrow}/>
             </Link>
         </div>
