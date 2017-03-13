170090eee0a4f08bca3831bfe2cd861e009bbe24
Evan Giese <giesee@mymacewan.ca>
2017-03-13 14:51:19 -0600

    made buttons prettier, margins are still weird

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -5,6 +5,7 @@ import ScatterplotMatrix from "./ScatterplotMatrix"
 import * as d3 from "d3";
 import {Button, ButtonGroup,  DropdownButton, MenuItem} from "react-bootstrap"
 import '../style/bootstrap/bootstrap.scss';
+import '../style/Nav.scss';
 
 const LArrow = {
     width: "16.66%",
@@ -22,32 +23,51 @@ const RArrow = {
 }
 
 const content = {
-width:"66.66%", 
-overflow: "scroll", 
-height:"500px",
-marginLeft: "16.6%",
-marginRight: "16.6%",
+    width:"66.66%", 
+    overflow: "scroll", 
+    height:"500px",
+    marginLeft: "16.6%",
+    marginRight: "16.6%",
 }
 
 const NavBar = {
     width: "100%",
     position: "fixed",
-    background
+    backgroundColor: "Black",
+    margin: 0
 
 };
+
+const myStyle = {
+    backgroundColor: "black",
+
+}
+
+const dropdownStyle = {
+    backgroundColor: "black",
+    zIndex : "0",
+    color: "white",
+}
+
 class Header extends Component{
 
   render() {
     return (
         <div>
+        
             <div style={NavBar}>
-                <ButtonGroup className="bootstrap">
-                    <DropdownButton title="Dropdown" id="bg-nested-dropdown" className="bootstrap">
-                        <MenuItem eventKey="1" className="bootstrap">Dropdown link</MenuItem>
-                        <MenuItem eventKey="2" className="bootstrap">Dropdown link</MenuItem>
+                <ButtonGroup className="bootstrap" style={myStyle}>
+                    <DropdownButton title="LESSONS" id="bg-nested-dropdown" className="" style={myStyle}>
+                        <MenuItem eventKey="1" className="" style={dropdownStyle}>GETTING STARTED</MenuItem>
+                        <MenuItem eventKey="2" className="" style={dropdownStyle}>JOB LIFE CYCLE</MenuItem>
+                        <MenuItem eventKey="3" className="" style={dropdownStyle}>BASIC IO</MenuItem>
+                        <MenuItem eventKey="4" className="" style={dropdownStyle}>THE TIME QUANTUM</MenuItem>
+                        <MenuItem eventKey="5" className="" style={dropdownStyle}>IO FREQUENCY AND PRIORITY</MenuItem>
+                        <MenuItem eventKey="6" className="" style={dropdownStyle}>PERSISTENT TIME QUANTUMS</MenuItem>
+                        <MenuItem eventKey="7" className="" style={dropdownStyle}>THE BOOST TIMER</MenuItem>
                     </DropdownButton>
-                    <Button className="bootstrap">{"<-"}</Button>
-                    <Button className="bootstrap">{"->"}</Button>
+                    <Button className="bootstrap glyphicon glyphicon-chevron-right" style={myStyle}></Button>
+                    <Button className="bootstrap glyphicon glyphicon-chevron-left" style={myStyle}></Button>
                 </ButtonGroup>
             </div>
 
