190b70df40b01b8836e767dd2177ee2fbaeafa0c
Spencer Killen <spleen_387@me.com>
2017-03-13 18:42:18 -0600

    header format

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -1,57 +1,46 @@
 
-import React, { Component } from 'react'
-import { Link } from 'react-router'
-import ScatterplotMatrix from "./ScatterplotMatrix"
-import "./Navigation.scss"
+import React, { Component } from "react";
+import { Link } from "react-router";
+import ScatterplotMatrix from "./ScatterplotMatrix";
 import * as d3 from "d3";
-
 
 const LArrow = {
     width: "16.66%",
     position: "fixed",
-    left:"0",
-    bottom:"50%",
-    top:"30%"
-}
+    left: "0",
+    bottom: "50%",
+    top: "30%"
+};
 const RArrow = {
     width: "16.66%",
     position: "fixed",
-    right:"0",
-    bottom:"50%",
-    top:"30%"
-}
-
-const content = {
-width:"66.66%", 
-overflow: "scroll", 
-height:"500px",
-marginLeft: "16.6%",
-marginRight: "16.6%",
-}
-
-
-class Header extends Component{
-
-  render() {
-    return (
-        <div>
-            <Link to={"Scheduler"}>
-                <img src="leftArrow.png" style={LArrow}/>
-            </Link>
-            <div style={content}>{this.props.children}</div>
-            <Link to={"Scheduler"}>
-                <img src="RightArrow.png" style={RArrow}/>
-            </Link>
-        </div>
-        
-    )
-  }
+    right: "0",
+    bottom: "50%",
+    top: "30%"
 };
 
+const content = {
+    width: "66.66%",
+    overflow: "scroll",
+    height: "500px",
+    marginLeft: "16.6%",
+    marginRight: "16.6%",
+};
 
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Link to={"Scheduler"}>
+                    <img src="leftArrow.png" style={LArrow} />
+                </Link>
+                <div style={content}>{this.props.children}</div>
+                <Link to={"Scheduler"}>
+                    <img src="RightArrow.png" style={RArrow} />
+                </Link>
+            </div>
+        );
+    }
+};
 
-
-
-
-
-export default Header
+export default Header;
