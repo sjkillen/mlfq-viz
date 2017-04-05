f2e3bf6a142e447d3e83b68410296c1c3ed69f52
Spencer Killen <spleen_387@me.com>
2017-04-05 17:46:43 -0600

    Nav bar switches lessons

--- src/views/Header.jsx
+++ src/views/Header.jsx
@@ -8,144 +8,110 @@ import '../style/Nav.scss';
 import DatGUI from './dat-gui';
 import { navigate } from "../data/guiActions";
 import lessons, { setLesson } from "../data/lessons"
+import HeaderStore from "../data/HeaderStore";
+import "./Header.scss";
 
+import { Container } from "flux/utils";
 
 const pathLArrow = require("./Images/leftArrow.png");
 const pathRArrow = require("./Images/RightArrow.png");
 
-
 const content = {
-   width: "80.00%",
-   height: "95%",
+    width: "80.00%",
+    height: "95%",
 }
 const myStyle = {
-   backgroundColor: "black",
-   color: "white",
+    backgroundColor: "black",
+    color: "white",
 }
 const dropdownStyle = {
-   backgroundColor: "black",
-   zIndex: "0",
-   color: "white",
+    backgroundColor: "black",
+    zIndex: "0",
+    color: "white",
 };
 
-
 class Header extends Component {
-    
+   static getStores() {
+      return [HeaderStore];
+   }
+   static calculateState(prevState) {
+      return HeaderStore.getState().toJS();
+   }
     lArrow() {
-        var view = this.props.location.pathname;
-        var disp = ""
+        const view = this.props.location.pathname;
+        let disp = ""
         if (view === "/Scheduler")
             disp = "none";
-        else if (view === "/SPLOM") 
+        else if (view === "/SPLOM")
             disp = "";
-        else if (view === "/PAAxis"){
+        else if (view === "/PAAxis") {
             disp = "";
         }
-        return {display: disp}
+        return { display: disp }
     }
 
     rArrow() {
-        var view = this.props.location.pathname;
-        var disp;
-        if (view === "/Scheduler") 
-            disp: "";
-        else if (view === "/SPLOM") 
+        const view = this.props.location.pathname;
+        let disp;
+        if (view === "/Scheduler")
+            disp =  "";
+        else if (view === "/SPLOM")
             disp = "";
-        else if(view === "/PAAxis")
+        else if (view === "/PAAxis")
             disp = "none";
 
-        return {display: disp}
+        return { display: disp }
     }
 
-    RarrowController () {
-        var currentView = this.props.location.pathname;
-         if (currentView === "/Scheduler")
+    RarrowController() {
+        const currentView = this.props.location.pathname;
+        if (currentView === "/Scheduler")
             return "SPLOM"
-        else if (currentView === "/SPLOM") 
+        else if (currentView === "/SPLOM")
             return "PAAxis"
     }
 
-    LarrowController () {
-        var currentView = this.props.location.pathname;
-         if (currentView === "/SPLOM")
+    LarrowController() {
+        const currentView = this.props.location.pathname;
+        if (currentView === "/SPLOM")
             return "Scheduler"
         else if (currentView === "/PAAxis")
             return "SPLOM"
     }
-
-    
-
-    configScheduler(){
-    }
-
-    configHandler (event) {
-        switch (event) {
-            case "0":
-                console.log("0!")  
-                break;
-            case "1":
-                console.log("1!")  
-                break;
-            case "2": 
-                console.log("2!!");
-                break;
-            case "3": 
-                console.log("3!!");
-                break;
-            case "4": 
-                console.log("4!!");
-                break;
-            case "5": 
-                console.log("5!!");
-                break;
-            case "6": 
-                console.log("6!!");
-                break;
-            case "7":
-                console.log("7!!");
-                break;
-            default:
-                break;
-        }
-    }
-
     render() {
         return (
-            
-            <div>
-            <DatGUI Hello="asdasdfasdfadf"/>
+            <div className="Header">
+                <DatGUI />
                 <div>
                     <ButtonGroup className="bootstrap" style={myStyle}>
-                        <DropdownButton title="LESSONS" id="bg-nested-dropdown" className="Nav" style={myStyle}>
-                            <MenuItem eventKey="0" className="MenuItem" onSelect={this.configHandler} style={dropdownStyle}>EXPLORE</MenuItem>
-                            <MenuItem eventKey="1" className="" onSelect={this.configHandler} style={dropdownStyle}>GETTING STARTED</MenuItem>
-                            <MenuItem eventKey="2" className="" onSelect={this.configHandler} style={dropdownStyle}>JOB LIFE CYCLE</MenuItem>
-                            <MenuItem eventKey="3" className="" onSelect={this.configHandler} style={dropdownStyle}>BASIC IO</MenuItem>
-                            <MenuItem eventKey="4" className="" onSelect={this.configHandler} style={dropdownStyle}>THE TIME QUANTUM</MenuItem>
-                            <MenuItem eventKey="5" className="" onSelect={this.configHandler} style={dropdownStyle}>IO FREQUENCY AND PRIORITY</MenuItem>
-                            <MenuItem eventKey="6" className="" onSelect={this.configHandler} style={dropdownStyle}>PERSISTENT TIME QUANTUMS</MenuItem>
-                            <MenuItem eventKey="7" className="" onSelect={this.configHandler} style={dropdownStyle}>THE BOOST TIMER</MenuItem>
+                        <DropdownButton title="LESSONS" id="bg-nested-dropdown" className="Nav" style={myStyle}>{
+                            Object.getOwnPropertyNames(lessons).map((lesson, i) => {
+                                const selected = this.state.selectedLesson === lessons[lesson].lessonName;
+                                const className = selected ? "menu selected" : "menu";
+                                return (
+                                    <MenuItem className={className} key={lesson} eventKey={lesson} onSelect={setLesson} style={dropdownStyle}>
+                                        {lessons[lesson].lessonName || lesson}
+                                    </MenuItem>
+                                )
+                            })}
                         </DropdownButton>
                         <Button className="bootstrap glyphicon glyphicon-chevron-left btn myBtn" style={myStyle} ></Button>
                         <div style={{ color: "white", display: "inline-block", width: "35px", textAlign: "center" }}>
                             {10}
                         </div>
-
                         <Button className="bootstrap glyphicon glyphicon-chevron-right btn myBtn" style={myStyle}></Button>
-                        
-                    
                     </ButtonGroup>
                 </div>
-                <Link to={this.LarrowController()} className="Nav" style = {this.lArrow()}>
-                    <img src={pathLArrow} className="myLArrow"/>
+                <Link to={this.LarrowController()} className="Nav" style={this.lArrow()}>
+                    <img src={pathLArrow} className="myLArrow" />
                 </Link>
                 <div style={content}>{this.props.children}</div>
-                <Link to={this.RarrowController()} className="Nav"  style = {this.rArrow()}>
-                    <img src={pathRArrow} className="myRArrow"/>
+                <Link to={this.RarrowController()} className="Nav" style={this.rArrow()}>
+                    <img src={pathRArrow} className="myRArrow" />
                 </Link>
             </div>
         );
     }
 }
 
-export default Header;
+export default Container.create(Header);
