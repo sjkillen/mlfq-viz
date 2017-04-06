fac96533b5c88f1c7a03e6dcacf3090280b99e72
Spencer Killen <spleen_387@me.com>
2017-04-05 18:26:03 -0600

    Freeze lessons

--- src/data/lessons.js
+++ src/data/lessons.js
@@ -5,16 +5,34 @@
 import dispatcher from "./dispatcher";
 
 const lessons = {
-   ["EXPLORE"]: require("./lessons/explore").default,
-   ["DUMB"]: require("./lessons/dumb").default,
-   ["GETTING STARTED"]: require("./lessons/dumb").default,
-   ["JOB LIFE CYCLE"]: require("./lessons/dumb").default,
-   ["BASIC IO"]: require("./lessons/dumb").default,
-   ["THE TIME QUANTUM"]: require("./lessons/dumb").default,
-   ["IO FREQUENCY AND PRIORITY"]: require("./lessons/dumb").default,
-   ["PERSISTENT TIME QUANTUMS"]: require("./lessons/dumb").default,
-   ["THE BOOST TIMER"]: require("./lessons/dumb").default
+   ["EXPLORE"]: getLesson("./lessons/explore"),
+   ["DUMB"]: getLesson("./lessons/dumb"),
+   ["GETTING STARTED"]: getLesson("./lessons/dumb"),
+   ["JOB LIFE CYCLE"]: getLesson("./lessons/dumb"),
+   ["BASIC IO"]: getLesson("./lessons/dumb"),
+   ["THE TIME QUANTUM"]: getLesson("./lessons/dumb"),
+   ["IO FREQUENCY AND PRIORITY"]: getLesson("./lessons/dumb"),
+   ["PERSISTENT TIME QUANTUMS"]: getLesson("./lessons/dumb"),
+   ["THE BOOST TIMER"]: getLesson("./lessons/dumb")
 };
+
+function getLesson(path) {
+   const obj = require(path).default;
+   deepFreeze(obj);
+   return obj;
+}
+
+/**
+ * Deeply freeze an obj
+ */
+function deepFreeze(obj) {
+   for (const key of Object.getOwnPropertyNames(obj)) {
+      if (typeof obj[key] === "object") {
+         deepFreeze(obj[key]);
+      }
+   }
+   Object.freeze(obj);
+}
 
 export default lessons;
 
