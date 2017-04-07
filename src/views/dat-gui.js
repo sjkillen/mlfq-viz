08356789c13ba994b856371f0ba9d6642d288282
Evan Giese <giesee@mymacewan.ca>
2017-04-06 22:05:49 -0600

    added height

--- src/views/dat-gui.js
+++ src/views/dat-gui.js
@@ -194,6 +194,7 @@ export default Container.createFunctional(datGui, () => [guiStore], () => {
     var menu2 = gui.addFolder("Scheduler Parameters"); 
       menu2.add(scheduler, 'Boost Time', 10, 100);
       menu2.add(scheduler, 'Trigger Boost')
+      menu2.add(SchedulerParametersPannel, "Scheduler Height", 1, 50)
       menu2.add(scheduler, 'Number of Queues',[1,2,3,4,5,6,7,8,9,10,11,12]);
       var Timequantum = menu2.addFolder("Time Quantums");
       for (var i = 1; i <= numberOfQues; i++)
