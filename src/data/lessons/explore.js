a8262116c0c71abb927ebaa64ce2b64730c29c8a
Evan Giese <giesee@mymacewan.ca>
2017-04-07 07:39:34 -0600

    add to explore

--- src/data/lessons/explore.js
+++ src/data/lessons/explore.js
@@ -56,28 +56,10 @@ export default {
       ]
    },
    parameter: {
-<<<<<<< HEAD
-      "render": true,
-      "Scheduler Parameters": {
-         "Boost Time": 100,
-         "Scheduler Height": 30,
-         "Number of Queues": 8,
-         "timeQuantums": [50, 5, 5, 5, 5, 5, 5, 5],
 
-      },
-      "Job Generator": {
-         "Duration": 10,
-         "Number of Jobs": 10,
-         "IO Frequency Min": 10,
-         'IO Frequency Max': 20,
-         "IO Length Min": 10,
-         "IO Length Max": 15,
-      },
-=======
         "render": true,
         "Scheduler Parameters": {
             "Boost Time": 100,
-            "Scheduler Height": 30,
             "Number of Queues": 12,
             "timeQuantums": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
             
@@ -90,7 +72,6 @@ export default {
             "IO Length Min" : 10,
             "IO Length Max" : 15,
         },
->>>>>>> 71a26cf061cb3f4d916e8839987cb2c518f727cc
    },
    details: {
       lesson: [
